ce97e6e3623c8b12dd2f39fd3eee90be5bbacfcc
alexis-pagnon <alexpagnon2003@gmail.com>
2025-09-15 16:52:09 +0200

    LS1
    Co-authored-by: AntoineJ165665 <AntoineJ165665@users.noreply.github.com>
    Co-authored-by: Simon D <SimonDum@users.noreply.github.com>
    Co-authored-by: Adam Sanchez <Doumsfx@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,9 +4,12 @@
 # 09/09/2025
 # --------------------------------------------------
 
+import time
 import docplex
 import docplex.mp
 import docplex.mp.model
+import random
+import copy
 
 # Ouvrir le fichier avec nos variables (code récupéré du projet de S8)
 airland_file = open("airlands/airland2.txt")
@@ -142,6 +145,7 @@ def decode2(sequence):
 
     return x, cost, True
 
+
 def decode_docplex(sequence):
     """
     Décode une séquence d'atterrissage en trouvant les temps optimaux
@@ -150,34 +154,33 @@ def decode_docplex(sequence):
     """
     mdl = docplex.mp.model.Model("TempsSequence")
     
-    # variables pour les temps d'atterrissage
+    # Variables pour les temps d'atterrissage
     x = {i: mdl.continuous_var(lb=E[i], ub=L[i], name=f"x_{i}") for r in sequence for i in r}
     
-    # variables pour avance et retard
+    # Variables pour avance et retard
     early = {i: mdl.continuous_var(lb=0, name=f"early_{i}") for r in sequence for i in r}
     late  = {i: mdl.continuous_var(lb=0, name=f"late_{i}") for r in sequence for i in r}
     
-    # contraintes d'avance/retard
     for r in sequence:
+        # Contraintes d'avance/retard
         for i in r:
             mdl.add_constraint(T[i] - x[i] == early[i] - late[i])
-    
-    # contraintes de précédence
-    for r in sequence:
+
+        # Contraintes de précédence
         for idx in range(1, len(r)):
             prev = r[idx-1]
             curr = r[idx]
             mdl.add_constraint(x[curr] >= x[prev] + s[prev][curr])
     
-    # fonction objectif : minimiser le coût
+    # Fonction objectif : minimiser le coût
     mdl.minimize(mdl.sum(c_minus[i]*early[i] + c_plus[i]*late[i] for r in sequence for i in r))
     
-    # résoudre
+    # Résolution
     solution = mdl.solve(log_output=False)
     if solution is None:
         return {}, float("inf"), False
     
-    # récupérer les temps optimaux
+    # Récupérer les temps optimaux
     x_opt = {i: x[i].solution_value for r in sequence for i in r}
     cost = mdl.objective_value
     
@@ -190,9 +193,10 @@ def decode_docplex(sequence):
 
 # print(decode_docplex([[2,4,7,8,0,13,12,1,11,10],[3,5,6,9,14]]))
 
-
+# print(decode_docplex([[0, 7, 3, 9, 8, 10, 2, 19, 1, 6, 14, 4, 23, 17, 13, 22, 16, 49, 25, 24, 42, 15, 21, 43, 48, 27, 31, 28, 46, 33, 37, 20, 38, 45, 35, 39, 41], [5, 11, 18, 12, 34, 26, 44, 32, 36, 47, 29, 30, 40]]))
 
 def create_initial_solution() :
+    """ Créer une solution initiale non optimisée (On prend la première qui est faisable) """
     sequence = [ [] for _ in range(m)]
     liste_avions = list(range(n))
     liste_avions.sort(key=lambda x: T[x])
@@ -221,3 +225,106 @@ def create_initial_solution() :
 
 
 # TODO : Générer voisins -> Local Search (swap, relocate, move_between_runways, stop criteria), VNS, Tabu Search
+# Possible d'implémenter l'approximation pour comparer avec docplex le résultat final
+
+def relocate_intra(seq, r, i, j)->list:
+    """ 
+    Retire l'avion d'indice 'i' de la piste 'r' et le réinsère sur celle-ci à l'indice 'j'.
+    Renvoie la nouvelle séquence trouvée.
+    """
+    new_seq = copy.deepcopy(seq)
+
+    if len(new_seq[r]) < 2:
+        return new_seq
+    
+    # On enlève l'avion i 
+    plane = new_seq[r].pop(i)
+    
+    # Puis on le replace à l'emplacement j
+    new_seq[r].insert(j, plane)
+
+    return new_seq
+
+
+def move_between(seq, r1, r2, i1, i2)->list:
+    """ 
+    Déplace l'avion d'indice i1 de la piste r1 vers la piste r2, à l'indice i2 
+    Renvoie la nouvelle séquence trouvée.
+    """
+    new_seq = copy.deepcopy(seq)
+
+    if len(new_seq) < 2:
+        return new_seq
+    
+    if not new_seq[r1]:
+        return new_seq
+    
+    # On enlève l'avion i1 sur la première piste r1
+    plane = new_seq[r1].pop(i1)
+
+    # Puis on le replace à l'emplacement i2 sur la seconde piste r2
+    new_seq[r2].insert(i2, plane)
+
+    return new_seq
+
+
+def LS1(seq, t_debut, t_max):
+    """ 
+    Algorithme de Local Search permettant à partir d'une séquence donnée de nous renvoyer la meilleure solution (séquence et coût) 
+    trouvée dans un temps imparti. Elle crée le voisinage en faisant toutes les combinaisaisons possibles pour chacunes des pistes indépendamment.
+    """
+                
+    best_seq = seq
+    _, initial_cost, _ = decode_docplex(best_seq)
+    best_cost = initial_cost
+
+    # Condition d'arrêt temporelle
+    if(t_debut + t_max < time.time()):
+        return best_seq, best_cost
+    
+    # On crée notre voisinage 
+    neighborhoods = []
+    for r in range(m):
+        for i in range(len(seq[r])):
+            for j in range(i+1, len(seq[r])):
+                neighborhoods.append(relocate_intra(seq, r, i, j))
+
+    # On calcul le coût de chacunes des séquences trouvées afin de garder la meilleure
+    for voisin in neighborhoods:
+        _, new_cost, new_feasible = decode_docplex(voisin)
+        if new_feasible and new_cost < best_cost:
+            best_seq, best_cost = voisin, new_cost
+
+    if(best_cost < initial_cost):
+        return LS1(best_seq, t_debut, t_max)
+    else:
+        return best_seq, best_cost
+
+
+# print(LS1([[2,7,8,0,13,4,11,1,12,10],[3,5,6,9,14]], time.time(), 10))
+
+
+
+
+
+
+def LS2(seq, max_iter = 100):
+    """ 
+    Algorithme de Local Search permettant à partir d'une séquence donnée de nous renvoyer la meilleure solution trouvée 
+    lorsque l'on atteint le nombre maximum d'itérations donné. Elle effectue à chaque fois les 3 types de voisnages différents.
+    """
+    best_seq = seq
+    _, best_cost, feasible = decode_docplex(best_seq)
+
+    # On regarde dans un premier temps si c'est faisable
+    if not feasible:
+        return best_seq, float("inf")
+    
+    # On cherche une solution optimale tant que nous n'avons pas atteint le max d'itérations souhaité
+
+
+    return best_seq, best_cost
+
+
+
+# Dans rapport, parler de l'approximation dans les limites.
